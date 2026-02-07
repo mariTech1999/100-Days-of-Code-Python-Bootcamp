0e516bc95d04d99b168bd2ecc49b69c4ad5d4ff0
Marina Keiko Yano <marinakeiko1999@gmail.com>
2026-02-07 07:28:29 -0300

    Projeto Band Name Generator

--- Dia 1/task.py
+++ Dia 1/task.py
@@ -1,3 +1,4 @@
+# Atualização
 print("Welcome to the Band Name Generator!")
 
 name = input("What is your name?\n")
@@ -5,5 +6,3 @@ city = input("What's the name of the city you grew up in?\n")
 pet = input("What's your pet's name?\n")
 
 print("Your band name could be " + city + " " + pet)
-
-# Atualização
