cf0b34a3f28e0ee07a7caae4a592e03e7851e423
Marina Keiko Yano <marinakeiko1999@gmail.com>
2026-03-23 21:25:28 -0300

    Update

--- Dia-026/nato/main.py
+++ Dia-026/nato/main.py
@@ -5,7 +5,16 @@ data = pandas.read_csv("nato-alphabet.csv")
 nato_dict = {row.Letra: row.Palavra for index, row in data.iterrows()}
 print(nato_dict)
 
-user_input = input("Enter a word: ").upper()
 
-output_list = [nato_dict[letter] for letter in user_input]
-print(output_list)
+
+while True:
+    user_input = input("Enter a word: ").upper()
+    try:
+        output_list = [nato_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry only letters a-z")
+    else:
+        print(output_list)
+        break
+
+
